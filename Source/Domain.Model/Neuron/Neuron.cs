79ae4649362b2af76736dede7fca3e5ddc94c0a9
Keyan Monadjem <ktmonadjem@gmail.com>
2023-03-15 19:54:51 +1300

    Add neuron file

---
+++ Source/Domain.Model/Neuron/Neuron.cs
@@ -0,0 +1,37 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Neuron
+{
+    public class Neuron
+    {
+        public double Output { get; }
+        public Matrix<double> Inputs { get; }
+        private Func<double, double> Activator { get; }
+
+        private Neuron(Matrix<double> inputs, Func<double, double> activator)
+        {
+            Inputs = inputs;
+            Activator = activator;
+        }
+
+        public static Neuron Create(Matrix<double> inputs, Func<double, double> activator)
+        {
+            return new Neuron(inputs, activator);
+        }
+
+        public double Activate()
+        {
+            return Activator(SumInputs());
+        }
+
+        private double SumInputs()
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ Tests/Tests/Domain.Model/NeuronTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Domain.Model
+{
+    [TestFixture]
+    public class NeuronTests
+    {
+
+    }
+}
