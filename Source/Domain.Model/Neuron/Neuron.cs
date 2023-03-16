79de43e57a08e5758c122a0ad6139aefa0f90dfd
Keyan Monadjem <ktmonadjem@gmail.com>
2023-03-17 10:13:59 +1300

    Updated neurons to allow graph construction

--- Source/Domain.Model/Neuron/Neuron.cs
+++ Source/Domain.Model/Neuron/Neuron.cs
@@ -4,33 +4,75 @@ namespace SupervisedLearning.ANN.Neuron
 {
     public class Neuron
     {
-        public double Output { get; private set; }
+        private bool _hasActivated = false;
+        private double _output;
+
+        public double Output { 
+            get 
+            {
+                if (!_hasActivated)
+                {
+                    Activate();
+                }
+                return _output;
+            }
+        }
         public double Bias { get; }
-        public Vector<double> Inputs { get; }
+        public Vector<double>? Inputs { get; }
         public Vector<double> Weights { get; }
         public Func<double, double> Activator { get; }
+        public List<Neuron>? Parents { get; }
 
-        private Neuron(Vector<double> inputs, Vector<double> weights, double bias, Func<double, double> activator)
+        private Neuron(Vector<double>? inputs, List<Neuron>? parents, Vector<double> weights, double bias, Func<double, double> activator)
         {
             Inputs = inputs;
+            Parents = parents;
             Weights = weights;
             Bias = bias;
             Activator = activator;
         }
 
-        public static Neuron Create(Vector<double> inputs, Vector<double> weights, double bias, Func<double, double> activator)
+        public static Neuron CreateWithParents(List<Neuron>parents, Vector<double> weights, double bias, Func<double, double> activator)
         {
-            return new Neuron(inputs, weights, bias, activator);
+            return new Neuron(null, parents, weights, bias, activator);
         }
 
-        public void Activate()
+        public static Neuron CreateWithInputs(Vector<double> inputs, Vector<double> weights, double bias, Func<double, double> activator)
         {
-            Output = Activator(SumInputs());
+            return new Neuron(inputs, null, weights, bias, activator);
+        }
+
+
+        public Vector<double> GetInputs()
+        {
+            if (Inputs is not null)
+            {
+                return Inputs;
+            }
+            else if (Parents is not null)
+            {
+                var inputs = new double[Parents.Count];
+                for (var i = 0; i < Parents.Count; i++)
+                {
+                    inputs[i] = Parents[i].Output;
+                }
+                return Vector<double>.Build.Dense(inputs);
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
         }
 
         private double SumInputs()
         {
-            return Inputs.PointwiseMultiply(Weights).Sum() + Bias;
+            return GetInputs().PointwiseMultiply(Weights).Sum() + Bias;
+        }
+
+        private void Activate()
+        {
+            _hasActivated = true;
+            _output = Activator(SumInputs());
         }
     }
 }
--- Tests/Tests/SupervisedLearning.ANN/NeuronTests.cs
+++ Tests/Tests/SupervisedLearning.ANN/NeuronTests.cs
@@ -11,32 +11,81 @@ namespace Tests.Domain.Model
     public class NeuronTests
     {
         private static readonly Vector<double> _inputs = 
-            Vector<double>.Build.Dense(new double[] { 0, 1, 2, 3, 4, 5 });
+            Vector<double>.Build.Dense(new double[] { 1, 2, 3, 4, 5 });
+
+        private static readonly Vector<double> _parent1Inputs = Vector<double>.Build.Dense(new double[] { 1, 2 });
+        private static readonly Vector<double> _parent1Weights = _parent1Inputs.Clone();
+        private static readonly Vector<double> _parent2Inputs = Vector<double>.Build.Dense(new double[] { 3, 4 });
+        private static readonly Vector<double> _parent2Weights = _parent2Inputs.Clone();
+        private static List<Neuron> _parents;
+
         private static readonly Vector<double> _weights = _inputs.Clone();
         private const double Bias = 10;
         private static readonly IActivationFunction _activator = new LinearActivator();
 
-        [Test]
-        public void CreateNeuron_Should_CreateWithCorrectValues()
+        [SetUp]
+        public void Setup()
         {
-            var neuron = Neuron.Create(_inputs, _weights, Bias, _activator.Activate);
+            var parent1 = Neuron.CreateWithInputs(_parent1Inputs, _parent1Weights, Bias, _activator.Activate);
+            var parent2 = Neuron.CreateWithInputs(_parent2Inputs, _parent2Weights, Bias, _activator.Activate);
+            _parents = new List<Neuron>
+            {
+                parent1, 
+                parent2
+            };
+        }
+
+        [Test]
+        public void CreateWithInputs_Should_CreateWithCorrectValues()
+        {
+            var neuron = Neuron.CreateWithInputs(_inputs, _weights, Bias, _activator.Activate);
             neuron.Inputs.Should().BeEquivalentTo(_inputs);
+            neuron.Parents.Should().BeNull();
             neuron.Weights.Should().BeEquivalentTo(_weights);
             neuron.Bias.Should().Be(Bias);
             neuron.Activator.Should().BeEquivalentTo(_activator.Activate);
         }
 
         [Test]
-        public void Neuron_Should_ActivateCorrectly()
+        public void CreateWithParents_Should_CreateWithCorrectValues()
         {
-            var neuron = Neuron.Create(_inputs, _weights, Bias, _activator.Activate);
-            neuron.Activate();
+            var weights = _weights.SubVector(0, _parents.Count);
+            var neuron = Neuron.CreateWithParents(_parents, weights, Bias, _activator.Activate);
+
+            neuron.Inputs.Should().BeNull();
+            neuron.Parents.Should().BeEquivalentTo(_parents);
+            neuron.Weights.Should().BeEquivalentTo(weights);
+            neuron.Bias.Should().Be(Bias);
+            neuron.Activator.Should().BeEquivalentTo(_activator.Activate);
+        }
+
+        [Test]
+        public void NeuronWithInputs_Should_ActivateCorrectly()
+        {
+            var neuron = Neuron.CreateWithInputs(_inputs, _weights, Bias, _activator.Activate);
             var output = neuron.Output;
 
             var expected = Bias;
             for (var i = 0; i < _inputs.Count; i++)
             {
                 expected += _inputs[i] * _weights[i];
+            }
+            expected = _activator.Activate(expected);
+
+            output.Should().Be(expected);
+        }
+
+        [Test]
+        public void NeuronWithParents_Should_ActivateCorrectly()
+        {
+            var weights = _weights.SubVector(0, _parents.Count);
+            var neuron = Neuron.CreateWithParents(_parents, weights, Bias, _activator.Activate);
+            var output = neuron.Output;
+
+            var expected = Bias;
+            for (var i = 0; i < _parents.Count; i++)
+            {
+                expected += _parents[i].Output * weights[i];
             }
             expected = _activator.Activate(expected);
 
