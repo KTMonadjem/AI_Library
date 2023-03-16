b5651e1cbbf2498024d4f47dcedc87e292ba2270
Keyan Monadjem <ktmonadjem@gmail.com>
2023-03-17 12:37:52 +1300

    More tests

--- Tests/Tests/SupervisedLearning.ANN/LayerTests.cs
+++ Tests/Tests/SupervisedLearning.ANN/LayerTests.cs
@@ -10,41 +10,28 @@ namespace Tests.SupervisedLearning.ANN
     public class LayerTests
     {
         private static readonly MatrixBuilder<double> M = Matrix<double>.Build;
-
-        private static readonly double[,] _weightInputs = new double[,]
-            {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 }
-            };
-        private static Matrix<double> _weights;
         private static readonly IActivationFunction _activator = new LinearActivator();
 
-        [SetUp]
-        public void SetUp()
+        [TestCaseSource(nameof(LayerDataSources))]
+        public void Create_Should_CreateCorrectly(double[,] weightInputs, int numberOfNeurons, int numberOfWeights)
         {
-            _weights = M.DenseOfArray(_weightInputs);
-        }
-
-        [Test]
-        public void Create_Should_CreateCorrectly()
-        {
-            var layer = Layer.Create(_weights, _activator);
-            layer.Weights.Should().BeEquivalentTo(M.DenseOfArray(_weightInputs));
+            var weightsMatrix = M.DenseOfArray(weightInputs);
+            var layer = Layer.Create(weightsMatrix, _activator);
+            layer.Weights.Should().BeEquivalentTo(M.DenseOfArray(weightInputs));
             layer.Activator.Should().BeEquivalentTo(_activator);
         }
 
-        [Test]
-        public void Build_Should_CorrectlyBuildNeurons()
+        [TestCaseSource(nameof(LayerDataSources))]
+        public void Build_Should_CorrectlyBuildNeurons(double[,] weightInputs, int numberOfNeurons, int numberOfWeights)
         {
-            var layer = Layer.Create(_weights, _activator).Build();
+            var weightsMatrix = M.DenseOfArray(weightInputs);
+            var layer = Layer.Create(weightsMatrix, _activator).Build();
 
-            layer.Neurons.Should().HaveCount(3);
-            var weights = _weightInputs.Cast<double>().ToList();
-            var weightsPerNeuron = weights.Count / 3;
-            for (var i = 0; i < 3; i++)
+            layer.Neurons.Should().HaveCount(numberOfNeurons);
+            var weights = weightInputs.Cast<double>().ToList();
+            for (var i = 0; i < numberOfNeurons; i++)
             {
-                var neuronWeights = weights.GetRange(i * 3, weightsPerNeuron);
+                var neuronWeights = weights.GetRange(i * numberOfWeights, numberOfWeights);
                 layer.Neurons[i].Weights.Should().BeEquivalentTo(neuronWeights.GetRange(1, neuronWeights.Count - 1));
                 layer.Neurons[i].Bias.Should().Be(neuronWeights[0]);
                 layer.Neurons[i].Activator.Should().Be(_activator.Activate);
@@ -52,5 +39,23 @@ namespace Tests.SupervisedLearning.ANN
                 layer.Neurons[i].Inputs.Should().BeNull();
             }
         }
+
+        public static object[] LayerDataSources =
+        {
+            new object[] { new double[,] {}, 0, 0},
+            new object[] { new double[,] { { 1, 2, 3 } }, 1, 3},
+            new object[] { new double[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            }, 3, 3},
+            new object[] { new double[,]
+            {
+                { 1, 2, 3, 0 },
+                { 4, 5, 6, 0 },
+                { 7, 8, 9, 0 }
+            }, 3, 4}
+        };
     }
 }
